36711be27894bc0a4520e4165625529514576e32
Zack Sorensen <zackjs21@byu.edu>
2024-02-24 16:00:41 -0700

    JS login

---
+++ startup_html/login.js
@@ -0,0 +1,7 @@
+function login() {
+const nameEl = document.querySelector("#username");
+localStorage.setItem("username", nameEl.value);
+console.log(nameEl.value);
+const passEl = document.querySelector("#password");
+localStorage.setItem("password", passEl.valiue);
+}
