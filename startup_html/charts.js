2f946630b3682c075e5deba18971b81d11ad4283
Zack Sorensen <zackjs21@byu.edu>
2024-02-24 23:14:52 -0700

    Add new PR functionality

--- startup_html/charts.js
+++ startup_html/charts.js
@@ -64,6 +64,7 @@ function Add_popup(option) {
 }
 
 
+
 function Add_data (option) {
 // get the distance and time inputted by the user - gotta wait till they input data
     const dselector = "#" + option + "_dist";
@@ -85,6 +86,40 @@ function Add_data (option) {
     let secondsEl = document.querySelector(sselector);
     localStorage.setItem("seconds", secondsEl.value);
     let seconds = Number(secondsEl.value);
+
+    const sec = ToSeconds(hours, minutes, seconds);
+    return [distance, sec, option]; // no se si va a funcionar
+}
+
+
+function MakeRow(dist, sec, option){
+    const sel = "#" + option + "s";
+    tableEl = document.querySelector(sel);
+    let time = 0;
+    if (sec > 3599){
+        time = FormatFromSec(sec);
+    }
+    else {
+        time = ShortFormatFromSec(sec);
+    }
+    const distance = dist + " miles";
+    const pace = CalculatePace(dist, sec);
+
+    const distEl = document.createElement('td');
+    const timeEl = document.createElement('td');
+    const paceEl = document.createElement('td');
+
+    distEl.textContent = distance;
+    timeEl.textContent = time;
+    paceEl.textContent = pace;
+
+    const rowEl = document.createElement('tr');
+    rowEl.appendChild(distEl);
+    rowEl.appendChild(timeEl);
+    rowEl.appendChild(paceEl);
+
+    tableEl.appendChild(rowEl);
+
 }
 
 function ToSeconds (hours, minutes, seconds) { // takes time format and converts to seconds
@@ -116,7 +151,7 @@ function FormatFromSec(sec) {
     
 }
 
-function MilePaceFromSec(sec) {
+function ShortFormatFromSec(sec) {
     
     let minutes = Math.floor(sec/60);
     let seconds = sec % 60;
@@ -136,9 +171,14 @@ function MilePaceFromSec(sec) {
 
 function CalculatePace(dist, sec) { // takes in dist and seconds, returns formatted pace per mile
     const pace = sec/ dist;
-    console.log(MilePaceFromSec(pace));
-    return MilePaceFromSec(pace);
+    
+    return ShortFormatFromSec(pace);
+}
 
+function Submit(option){
+    const a = Add_data(option);
+    MakeRow(...a)
+    console.log("Success I hope")
 }
 
 // how to make the popups disappear if click anywhere else? 
