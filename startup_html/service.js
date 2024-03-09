25304e885a4d923db78e62ad656c03d6731afa40
Zack Sorensen <zackjsorensen@gmail.com>
2024-03-09 10:37:07 -0700

    Starting out, serving up files from public

---
+++ startup_html/service.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+// The service port. In production the frontend code is statically hosted by the service on the same port.
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+// JSON body parsing using built-in middleware
+app.use(express.json());
+
+// Serve up the frontend static content hosting
+app.use(express.static('public'));
