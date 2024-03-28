52c14ed719a7fa58c557c1a3955cda59c4f3c104
zackjsorensen <zackjsorensen@gmail.com>
2024-03-27 21:16:28 -0600

    Starting login call

--- startup_html/database.js
+++ startup_html/database.js
@@ -1,14 +1,17 @@
 
 const { MongoClient, MongoDBNamespace } = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
 const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
-
+// const url = 'ongodb+srv://zackjs:dummypass@cluster0.lq6ddnj.mongodb.net/?';
 
 const client = new MongoClient(url);
 const db = client.db('startup');
+const coll = db.collection('startup');
 
-
+console.log(1+1);
 
 (async function testConnection() {
   await client.connect();
@@ -21,19 +24,25 @@ const db = client.db('startup');
 // functions interacting with mongodb
 
 // getuser(email)
+function getUser(email){
+
+}
 
 // getuserbytoken(token) -- cookie shortcut
 
 //createuser(email, password)
+function createUser(email, password) {
+
+}
 
 // getUserStats
 function getUserStats(username) {
-  return db.findOne({username: username});
+  return coll.findOne({username: username});
 
 }
 
 function tester() {
-  console.log(db.findOne({username: "dummy"}));
+  console.log(coll.findOne({username: "dummy"}));
   
 }
 //add pr
@@ -47,3 +56,7 @@ function tester() {
 //remove goal
 
 //get goals
+
+module.exports = {
+  getUserStats
+};
--- startup_html/service.js
+++ startup_html/service.js
@@ -17,45 +17,68 @@ app.use(express.json());
 // Serve up the frontend static content hosting
 app.use(express.static('public'));
 const apiRouter = express.Router();
-app.use(`/api`, goalRouter);
+app.use(`/api`, apiRouter);
 // const prRouter = express.Router();
 // app.use('/pr', prRouter);
 
 // Get, post goals
 
+// CreateAuth token for a new user
+apiRouter.post('/auth/create', async (req, res) => {
+  if (await DB.getUser(req.body.email)) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    const user = await DB.createUser(req.body.email, req.body.password);
+
+    // Set the cookie
+    setAuthCookie(res, user.token);
+
+    res.send({
+      id: user._id,
+    });
+  }
+});
 
 //test router to return stats given an email
 apiRouter.get('/stats', (_req, res) => {
   const username = _req.body.email
   const stats = DB.getUserStats(username);
   console.log(stats);
+  res.send("Testing Testing"); // works
 });
 
-goalRouter.post('/', (_req, res) => {
-  // receive and update here
-  console.log("Goals saved to service memory:");
+apiRouter.get('/test', async(_req, res) => {
+  const holder = await DB.getUserStats("dummy")
+  console.log(holder);
+  res.send(holder);
+});
+
+
+// goalRouter.post('/', (_req, res) => {
+//   // receive and update here
+//   console.log("Goals saved to service memory:");
  
-  goals = (_req.body);
-  console.log(goals);
+//   goals = (_req.body);
+//   console.log(goals);
   
-  res.send(goals);
-});
+//   res.send(goals);
+// });
 
-// Get, post prs
+// // Get, post prs
 
-prRouter.get('/', (_req, res) => {
-  res.send(prs)
-  });
+// prRouter.get('/', (_req, res) => {
+//   res.send(prs)
+//   });
   
-  prRouter.post('/', (_req, res) => {
-    // receive and update here
-    console.log("PRs saved to service memory:");
+//   prRouter.post('/', (_req, res) => {
+//     // receive and update here
+//     console.log("PRs saved to service memory:");
    
-    prs = (_req.body);
-    console.log(prs);
+//     prs = (_req.body);
+//     console.log(prs);
     
-    res.send(prs);
-  });
+//     res.send(prs);
+//   });
 
 
 app.listen(port, () => {
