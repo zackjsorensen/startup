7f289e596684c8a844d0e6707c21cd963cd4eedd
zackjsorensen <zackjsorensen@gmail.com>
2024-04-12 22:17:49 -0600

    React: Header & Footer inserted

--- startup_html/src/app.jsx
+++ startup_html/src/app.jsx
@@ -1,6 +1,34 @@
 import React from 'react';
 import './app.css';
 
+// TODO: Fix Login/logout system
+
 export default function App() {
-    return <div className='body'>App will Display Here</div>;
+    return (
+        <div className='body'>
+            <header >
+                <div id="banner">
+                    <h1 id="logo"><em>PR318</em></h1>
+                </div>
+                <nav>
+                    <menu>
+                        <a href="login.html" className="navlink">Login</a>
+                        <a href="charts.html" className="navlink">My Stats</a>
+                        <a href="feed.html" className="navlink">Feed</a>
+                    </menu>
+                </nav>
+            </header>
+
+            <main>App Components Go Here</main>
+
+            <footer>
+                <p className="pfoot">Author: Zack Sorensen</p>
+                <p className="pfoot"><a id="githubfoot" href="https://github.com/zackjsorensen/startup">Github</a></p>
+
+            </footer>
+
+        </div>
+
+
+    );
 }
