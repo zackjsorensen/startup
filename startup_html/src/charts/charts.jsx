911a499bbccdb53582bd6a561c6ae68bbcb2fbb4
zackjsorensen <zackjsorensen@gmail.com>
2024-04-14 15:16:32 -0600

    Data retrieval working great

--- startup_html/src/charts/charts.jsx
+++ startup_html/src/charts/charts.jsx
@@ -7,29 +7,46 @@ export function Stats({ authState }) {
     const [prs, setPrs] = React.useState([]);
     const g = "goal";
     const p = "pr";
+    //  option is pr or goal, setData is the setGoals or setPrs, data is prs or goals
+    React.useEffect(() => {
+        fetch(`/api/goals`)
+            .then((response) => response.json())
+            .then((new_goals) => {
+                setGoals(new_goals);
+                localStorage.setItem(`goals`, JSON.stringify(goals));
+            })
+            .catch(() => {
+                console.log("Error: fetch request failed");
+                const goalText = localStorage.getItem(`goals`);
+                if (goalText) {
+                    setGoals(JSON.parse(goalText));
+                }
+            })
+    }, [])
 
-    function Retrieve(option, data, setData) {   // option is pr or goal, setData is the setGoals or setPrs, data is prs or goals
-        React.useEffect(() => {
-            console.log(`/api/${option}s`);
-            fetch(`/api/${option}s`)
-                .then((response) => response.json())
-                .then((new_data) => {
-                    setData(new_data);
-                    localStorage.setItem(`${option}s`, JSON.stringify(data));
-                })
-                .catch(() => {
-                    console.log("Error: fetch request failed");
-                    const dataText = localStorage.getItem(`${option}s`);
-                    if (dataText) {
-                        setData(JSON.parse(dataText));
-                    }
-                })
-        }, [])
-    }
+    React.useEffect(() => {
+        fetch(`/api/prs`)
+            .then((response) => response.json())
+            .then((new_prs) => {
+                setPrs(new_prs);
+                localStorage.setItem(`prs`, JSON.stringify(prs));
+            })
+            .catch(() => {
+                console.log("Error: fetch request failed");
+                const goalText = localStorage.getItem(`prs`);
+                if (prText) {
+                    setPrsJSON.parse(prText);
+                }
+            })
+    }, [])
+
+
+
+
 
     if (authState === AuthState.Authenticated) {
-         Retrieve(g, goals, setGoals); // fetches data, sets goals to what is returned
-         Retrieve(p, prs, setPrs);
+        //  Retrieve(g, goals, setGoals); // fetches data, sets goals to what is returned
+        //  Retrieve(p, prs, setPrs);
 
         console.log(JSON.stringify(goals));
         console.log(JSON.stringify(prs));
@@ -42,6 +59,17 @@ export function Stats({ authState }) {
                     <td>{goal[0]}</td>
                     <td>{goal[1]}</td>
                     <td>{goal[2]}</td>
+                </tr>
+            );
+        }
+
+
+        for (const [i, pr] of prs.entries()) {
+            prRows.push(
+                <tr key={i}>
+                    <td>{pr[0]}</td>
+                    <td>{pr[1]}</td>
+                    <td>{pr[2]}</td>
                 </tr>
             );
         }
@@ -62,13 +90,14 @@ export function Stats({ authState }) {
                     </table>
                 </div>
                 <div id="pr_div" className="stat_div">
-                <h2 className = "seg_headers">My Goals</h2>
+                    <h2 className="seg_headers">My Goals</h2>
                     <table id="pr" className="stat_table">
-
-                        <thead className="table_hdrs">
-                            <th>Distance</th>
-                            <th>Time</th>
-                            <th>Pace</th>
+                        <thead>
+                            <tr className="table_hdrs">
+                                <th>Distance</th>
+                                <th>Time</th>
+                                <th>Pace</th>
+                            </tr>
                         </thead>
                         <tbody>{goalRows}</tbody>
                     </table>
